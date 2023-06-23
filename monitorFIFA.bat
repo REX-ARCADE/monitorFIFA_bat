b956ec529eabf5553a2dc6012ed248d99cc0283a
holarex <hola@rex-arcade.com>
2023-06-23 14:11:34 +0200

    Reset the PC if it tries 2 times and it really not starting the game

--- monitorFIFA.bat
+++ monitorFIFA.bat
@@ -1,12 +1,31 @@
 @echo off
+setlocal enabledelayedexpansion
+
+:: Inicializa el contador
+set contador=0
+
 :loop
 tasklist | find /i "FIFA23.exe" > nul 2>&1
 if errorlevel 1 (
-	echo "Ejecutando FIFA 23..."
-	start "" "C:\Program Files\EA Games\FIFA 23\FIFA23.exe"
-	timeout /t 30 /nobreak > nul
+    :: Incrementa el contador si FIFA23.exe no se está ejecutando
+    set /a contador=!contador!+1
+
+    echo "Ejecutando FIFA 23..."
+    start "" "C:\Program Files\EA Games\FIFA 23\FIFA23.exe"
+    timeout /t 30 /nobreak > nul
 ) else (
-	REM echo "El programa esta en ejecucion."
-	timeout /t 20 /nobreak > nul	
+    :: Restablece el contador si FIFA23.exe se está ejecutando
+    set contador=0
+
+    REM echo "El programa esta en ejecucion."
+    timeout /t 20 /nobreak > nul	
 )
+
+:: Comprueba si el contador ha alcanzado 2
+if !contador! equ 2 (
+    echo "El contador ha alcanzado el nº de intentos máxiomo, reiniciando la PC en 5 segundos..."
+    timeout /t 5 /nobreak > nul
+    shutdown /r /t 0
+)
+
 goto loop
